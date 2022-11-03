39109a36c12aafecd7f5574175242749d0f286fe
Youngwon Kim <youngwon0330@gmail.com>
2022-11-03 10:17:50 -0700

    dsnjnd

---
+++ scripts/navbar.js
@@ -0,0 +1,27 @@
+// hide login/signup button if user logged in
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        $('#loginSignupButton').addClass('loggedIn');
+        $('#logoutButton').css('display', 'inline-block');
+    } else {
+        $('#loginSignupButton').removeClass('loggedIn');
+        $('#logoutButton').css('display', 'none');
+    }
+});
+
+function goToLoginPage() {
+    window.location.href = './Login.html';
+}
+
+function logout() {
+    firebase
+        .auth()
+        .signOut()
+        .then(function () {
+            console.log('logout successful');
+            window.location.assign('index.html');
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
