ba2d34664d3c6f618ee2eb0c97f85c46f81e1943
gurtejx <gurtejmalik@gmail.com>
2022-11-24 09:28:14 -0800

    working

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,63 @@
+// firebase.auth().onAuthStateChanged(user => {
+//     if (user) {
+//         var currentUser = db.collection("users").doc(user.uid)
+//         var userID = user.uid;
+//         //get the document for current user.
+//         currentUser.get()
+//             .then(userDoc => {
+//                 var userEmail = userDoc.data().email;
+//                 db.collection("Reviews").add({
+//                     code: hikeID,
+//                     userID: userID,
+//                     title: Title,
+//                     level: Level,
+//                     season: Season,
+//                     description: Description,
+//                     flooded: Flooded,
+//                     scrambled: Scrambled,
+//                     timestamp: firebase.firestore.FieldValue.serverTimestamp()
+//                 }).then(()=>{
+//                     window.location.href = "thanks.html"; //new line added
+//                 })
+//             })
+               
+//     } else {
+//         // No user is signed in.
+//     }
+// });
+
+// firebase.auth().onAuthStateChanged(user => {
+//     // Check if user is signed in:
+//     if (user) {
+
+//         //go to the correct user document by referencing to the user uid
+//         currentUser = db.collection("posts").doc(post.uid)
+//         //get the document for current user.
+//         currentUser.get()
+        
+//         .then(userDoc => {
+//                 //get the data fields of the user
+//                 var activity = userDoc.data().details;
+//                 var location = userDoc.data().location;
+//                 var time = userDoc.data().time;
+
+//                 //if the data fields are not empty, then write them in to the form.
+//                 if (userName != null) {
+//                     document.getElementById("activityInput").value = activity;
+//                 }
+//                 if (userSchool != null) {
+//                     document.getElementById("locationlInput").value = location;
+//                 }
+//                 if (userCity != null) {
+//                     document.getElementById("timeInput").value = time;
+//                 }
+//             })
+//     } else {
+//         // No user is signed in.
+//         console.log ("No user is signed in");
+//     }
+// });
+
 function insertName() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if a user is signed in:
