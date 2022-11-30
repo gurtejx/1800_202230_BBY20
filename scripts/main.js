6699605da66a9a86de5de0ff03f3307ab83a0a01
Youngwon Kim <youngwon0330@gmail.com>
2022-11-30 15:08:49 -0800

    sdad

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,23 @@
 function insertName() {
-  firebase.auth().onAuthStateChanged((user) => {
-    // Check if a user is signed in:
-    if (user) {
-      // Do something for the currently logged-in user here:
-      console.log(user.uid);
-      console.log(user.displayName);
-      user_Name = user.displayName;
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) {
+            // Do something for the currently logged-in user here: 
+            console.log(user.uid);
+            console.log(user.displayName);
+            user_Name = user.displayName;
 
-      //method #1:  insert with html only
-      //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
-      //method #2:  insert using jquery
-      $("#name-goes-here").text(user_Name); //using jquery
-    } else {
-      // No user is signed in.
-      window.location.href = "login.html";
-      alert("No user is signed in. Access denied.");
-    }
-  });
+            //method #1:  insert with html only
+            //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
+            //method #2:  insert using jquery
+            $("#name-goes-here").text(user_Name); //using jquery
+
+        } else {
+            // No user is signed in.
+            window.location.href = "login.html";
+            alert ("No user is signed in. Access denied.");
+        }
+    });
 }
 insertName(); //run the function
 
-function deleteButton() {
-  firebase.auth().onAuthStateChanged((user) => {
-    // Check if a user is signed in:
-    if (user) {
-      // Do something for the currently logged-in user here:
-      // console.log(user.uid);
-      user_Name = user.uid;
-
-      // now check each post
-      db.collection("posts")
-        .orderBy("postDate", "desc")
-        .get()
-        .then((result) => {
-          result.forEach((doc) => {
-            if (doc.data().owner == user_Name) {
-                const button = document.querySelector('.deleteButton');
-                button.setAttribute("display", "block");
-            }
-
-          });
-        });
-    }
-  });
-}
-deleteButton();
