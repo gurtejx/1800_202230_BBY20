a6692c3a830f6847ac70a41c485c71137ff3d6da
Viet Hai Nguyen <vnguyen141@my.bcit.ca>
2022-11-30 14:13:28 -0800

    Viet

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-
 db.collection("posts")
 .orderBy("postDate","desc")
   .get()
@@ -14,9 +13,12 @@ db.collection("posts")
         <p class="card-text">${doc.data().content}<br><b>Event takes place at:</b> ${doc.data().date}<span>,</span> ${doc.data().time}
         <br><b>Email address: </b> ${doc.data().email}<br></p>
         <a href="http://gmail.com" target="_blank" class="btn btn-primary">Try Contact</a>
-        
+        <a class="btn btn-danger deletepost">Delete Post</a>
       </div>
     </div>`
       $('.container').append(template)
     });
   });
+
+
+
