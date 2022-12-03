b14efc66fa0c0417f43e7c2c33787ce2e381c9ec
Viet Hai Nguyen <vnguyen141@my.bcit.ca>
2022-12-02 22:00:24 -0800

    Viet

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-db.collection("posts")
-.orderBy("postDate","desc")
-  .get()
-  .then((result) => {
-    result.forEach((doc) => {
-      console.log(doc.data());
-      var template = `<div class="card">
-      <div class="card-header  modified-header">
-      ${doc.data().title}
-      </div>
-      <div class="card-body">
-        <h5 class="card-title">${doc.data().activity}</h5>
-        <p class="card-text">${doc.data().content}<br><b>Event takes place at:</b> ${doc.data().date}<span>,</span> ${doc.data().time}
-        <br><b>Email address: </b> ${doc.data().email}<br></p>
-        <a href="http://gmail.com" target="_blank" class="btn btn-primary">Try Contact</a>
-        <a class="btn btn-danger deletepost" id="deletebutton">Delete Post</a>
-      </div>
-    </div>`
-      $('.container').append(template)
-    });
-  });
-
-
-
-  
-=======
 // 
 /**
  * When user enter the content in the post page, the data will be populate into a card template below.
@@ -51,4 +24,3 @@ db.collection("posts")
      $('.container').append(template)
    });
  });
->>>>>>> 94c60470d47f17bc3da8aeeb64808df00596d390
